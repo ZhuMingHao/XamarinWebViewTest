cb8df480054b508b4976c41dcb21aaa25cbca77b
Nico Zhu <v-zhumin@microsoft.com>
2018-05-10 17:36:31 +0800

    Add project files.

---
+++ App5/App5.UWP/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace App5.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new App5.App());
+
+         
+        }
+        private async void GetUser()
+        {
+            IReadOnlyList<User> users = await User.FindAllAsync();
+
+            var current = users.Where(p => p.AuthenticationStatus == UserAuthenticationStatus.LocallyAuthenticated &&
+                                        p.Type == UserType.LocalUser).FirstOrDefault();
+
+            // user may have username
+            var data = await current.GetPropertyAsync(KnownUserProperties.AccountName);
+            string displayName = (string)data;
+
+            // authinticated using hotmail 
+            if (String.IsNullOrEmpty(displayName))
+            {
+
+                string a = (string)await current.GetPropertyAsync(KnownUserProperties.FirstName);
+                string b = (string)await current.GetPropertyAsync(KnownUserProperties.LastName);
+                displayName = string.Format("{0} {1}", a, b);
+            }
+        }
+    }
+}
---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var urlSource = new UrlWebViewSource { Url = "https://htmlpreview.github.io/?https://github.com/joelbyrd/external-resources/blob/master/storefulfillment/Printing/Packing/GNC-Pick.htm" };
+            var htmlStringSource = new HtmlWebViewSource
+            {
+                Html = @"<!doctype html><html lang='en'><head><meta charset='utf-8'><meta http-equiv='x-ua-compatible' content='ie=edge'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Test</title></head><body><div id='log'></div><p>some text</p><script>document.getElementById('log').innerHTML += `<p>Testing</p>`;</script></body></html>"
+            };
+
+            var webView = new WebView
+            {
+                Margin = new Thickness(0, 20, 0, 0),
+                //Source = urlSource, // displays as expected
+                Source = htmlStringSource // <script>'s are processed twice, appending "Testing" to the body twice
+            };
+
+            Content = webView;
+        }
+
+    }
+}
